d9db6da675908fb125ec68f924eba0f8abff4990
shinyay <shinya.com@gmail.com>
2021-02-23 22:47:27 +0900

    Initial Commit

---
+++ backend/src/main/kotlin/com/google/shinyay/SpringBootVueGsApplication.kt
@@ -0,0 +1,11 @@
+package com.google.shinyay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootVueGsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootVueGsApplication>(*args)
+}
