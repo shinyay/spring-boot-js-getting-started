bf3001d26f5b3c85ca640c87bf8e55c410c23466
shinyay <shinya.com@gmail.com>
2021-02-26 11:22:38 +0900

    Add current time API for GET Method

--- backend/src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ backend/src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -1,8 +1,11 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 @RestController
@@ -10,7 +13,10 @@ import java.time.format.DateTimeFormatter
 class HelloController {
 
     @GetMapping("/hello")
-    fun hello() {
+    fun hello(): String? {
         val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormat)
+        logger.info("Current Time: $currentTime")
+        return currentTime
     }
 }
