10f7931c69a0c059efe734cb998cb5d9ae36aa4d
shinyay <shinya.com@gmail.com>
2021-02-27 18:23:30 +0900

    Add find all users function

--- backend/src/main/kotlin/com/google/shinyay/service/UserService.kt
+++ backend/src/main/kotlin/com/google/shinyay/service/UserService.kt
@@ -1,8 +1,13 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.entity.User
 import com.google.shinyay.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(val repository: UserRepository) {
+
+    fun getUsers(): MutableIterable<User> {
+        return repository.findAll()
+    }
 }
