70e2bec880c2c16818e9a8dc3eaf2202b1e2ff21
shinyay <shinya.com@gmail.com>
2021-02-28 16:15:22 +0900

    Add UserService Injection

--- backend/src/main/kotlin/com/google/shinyay/controller/UserController.kt
+++ backend/src/main/kotlin/com/google/shinyay/controller/UserController.kt
@@ -1,9 +1,16 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.service.UserService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
-class UserController {
+class UserController(val service: UserService) {
+
+    @GetMapping("/users")
+    fun getUsers() {
+
+    }
 }
