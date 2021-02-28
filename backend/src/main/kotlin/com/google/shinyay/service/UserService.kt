1bb3faecc1d55c5c3ad63e380f674b488a7fa100
shinyay <shinya.com@gmail.com>
2021-02-28 15:52:24 +0900

    Add find user by ID

--- backend/src/main/kotlin/com/google/shinyay/service/UserService.kt
+++ backend/src/main/kotlin/com/google/shinyay/service/UserService.kt
@@ -3,6 +3,7 @@ package com.google.shinyay.service
 import com.google.shinyay.entity.User
 import com.google.shinyay.repository.UserRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserService(val repository: UserRepository) {
@@ -13,5 +14,9 @@ class UserService(val repository: UserRepository) {
 
     fun registerUser(user: User): User {
         return repository.save(user)
+    }
+
+    fun getUser(id: Long): Optional<User> {
+        return repository.findById(id)
     }
 }
