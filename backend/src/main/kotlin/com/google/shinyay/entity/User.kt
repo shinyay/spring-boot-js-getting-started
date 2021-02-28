462d37267c61dc09a6861a25e5c87c07a594e748
shinyay <shinya.com@gmail.com>
2021-02-28 17:41:57 +0900

    Add Entity annotation for JPA

--- backend/src/main/kotlin/com/google/shinyay/entity/User.kt
+++ backend/src/main/kotlin/com/google/shinyay/entity/User.kt
@@ -1,9 +1,11 @@
 package com.google.shinyay.entity
 
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
+@Entity
 data class User(@Id
                 @GeneratedValue(strategy = GenerationType.AUTO)
                 var id: Long,
