12ad0b42e673886e20b76e7db0b725a5e096e713
shinyay <shinya.com@gmail.com>
2021-02-28 16:31:15 +0900

    Add find user by ID fir GET Mapping

--- backend/src/main/kotlin/com/google/shinyay/controller/UserController.kt
+++ backend/src/main/kotlin/com/google/shinyay/controller/UserController.kt
@@ -1,10 +1,13 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.entity.User
+import com.google.shinyay.logger
 import com.google.shinyay.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,5 +16,11 @@ class UserController(val service: UserService) {
     @GetMapping("/users")
     fun getUsers(): MutableIterable<User> {
         return service.getUsers()
+    }
+
+    @GetMapping("users/{id}")
+    fun getUser(@PathVariable id: Long): Optional<User> {
+        logger.info("Find ID: $id")
+        return service.getUser(id)
     }
 }
