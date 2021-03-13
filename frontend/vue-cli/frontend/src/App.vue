a65831d738ae66a4411165fefbe9c0fba34dc21a
shinyay <shinya.com@gmail.com>
2021-03-13 18:22:22 +0900

    Modify syntax

--- frontend/vue-cli/frontend/src/App.vue
+++ frontend/vue-cli/frontend/src/App.vue
@@ -1,53 +1,47 @@
 <template>
   <v-app id="app">
-    <v-img
-      :src="require('./assets/logo.svg')"
-      class="my-3"
-      contain
-      height="200"
-    />
-    <div class="interface">
-      <v-navigation-drawer permanent color="transparent">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>
-            Content
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+    <v-container>
+      <!-- <v-row class="text-center">
+        <v-col cols="12">
+          <v-img
+            :src="require('./assets/logo.svg')"
+            class="my-3"
+            contain
+            height="200"
+          />
+        </v-col>
+      </v-row> -->
 
-      <v-divider></v-divider>
+      <div class="interface">
+        <v-navigation-drawer permanent color="transparent">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                Content
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item>
-          <v-list-item-title><router-link to="/list" class="linkpath">ユーザーリスト</router-link></v-list-item-title>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-title><router-link to="/new" class="linkpath">新規ユーザー</router-link></v-list-item-title>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-title><router-link to="/hello" class="linkpath">Hello World</router-link></v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <router-view />
-    </div>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <!-- <v-img
-          :src="require('./assets/logo.png')"
-          class="my-3"
-          contain
-          height="200"
-        /> -->
-      </v-col>
-    </v-row>
+          <v-divider></v-divider>
+
+          <v-list dense nav >
+            <v-list-item>
+              <v-list-item-title><router-link to="/list" class="linkpath">ユーザーリスト</router-link></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title><router-link to="/new" class="linkpath">新規ユーザー</router-link></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title><router-link to="/hello" class="linkpath">Hello World</router-link></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      <router-view />
+      </div>
     <!-- <v-main>
       <HelloWorld/>
     </v-main> -->
+    </v-container>
   </v-app>
 </template>
 
