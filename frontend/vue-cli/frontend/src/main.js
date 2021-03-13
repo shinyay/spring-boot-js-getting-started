3ee996a2bcbd3141d46c0b971350c95b719c5fd0
shinyay <shinya.com@gmail.com>
2021-03-13 14:32:54 +0900

    Add Vuetify installation

--- frontend/vue-cli/frontend/src/main.js
+++ frontend/vue-cli/frontend/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import vuetify from "./plugins/vuetify";
+import App from "./App";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+
+app.mount("#app");
