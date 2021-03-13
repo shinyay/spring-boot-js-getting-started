befc2443d9effce88a60e86f5a27a17b1aaf3e5b
shinyay <shinya.com@gmail.com>
2021-03-13 15:11:47 +0900

    Add router to app

--- frontend/vue-cli/frontend/src/main.js
+++ frontend/vue-cli/frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import vuetify from "./plugins/vuetify";
 import App from "./App";
+import router from './router'
 
 const app = createApp(App);
 
 app.use(vuetify);
-
+app.use(router);
 app.mount("#app");
