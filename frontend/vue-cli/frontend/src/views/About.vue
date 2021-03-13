318b732a16761804bb989e42e07bb721b1e9319f
shinyay <shinya.com@gmail.com>
2021-03-13 11:28:00 +0900

    Add fetching data from backend service

--- frontend/vue-cli/frontend/src/views/About.vue
+++ frontend/vue-cli/frontend/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/api/v1/hello")
+    .then((response) => response.text())
+    .then((data) => {
+      this.msg = data;
+    });
+  }
+}
+</script>
