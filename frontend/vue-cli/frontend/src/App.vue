a97c5506c43075540037f5dbcd16ea576344981b
shinyay <shinya.com@gmail.com>
2021-03-13 09:21:49 +0900

    Add style for Vue app

--- frontend/vue-cli/frontend/src/App.vue
+++ frontend/vue-cli/frontend/src/App.vue
@@ -14,5 +14,19 @@ export default {
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 30px;
+}
+.linkpath {
+  margin: 0 10px;
+}
+.interface {
+  display: flex;
+  justify-content: center;
+}
 </style>
