47445f8f1442ed1e94ed9f264f6e715f10a2868f
shinyay <shinya.com@gmail.com>
2021-03-13 10:52:28 +0900

    Add frontend project with VueR outer generated by Vue CLI

--- frontend/vue-cli/frontend/src/App.vue
+++ frontend/vue-cli/frontend/src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#nav {
   padding: 30px;
 }
-.linkpath {
-  margin: 0 10px;
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-.interface {
-  display: flex;
-  justify-content: center;
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
