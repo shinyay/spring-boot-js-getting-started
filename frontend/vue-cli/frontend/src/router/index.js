dc7159da875ecff4647142483fa0fd1f6a50efa0
shinyay <shinya.com@gmail.com>
2021-03-14 22:53:22 +0900

    Modify links to userlist and userform

--- frontend/vue-cli/frontend/src/router/index.js
+++ frontend/vue-cli/frontend/src/router/index.js
@@ -1,35 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
-import Hello from '../components/HelloWorld.vue'
+import UserList from '../views/UserList.vue'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'List',
+    component: UserList
   },
   {
     path: '/new',
-    name: 'Form',
+    name: 'UserForm',
     component: () => import('../views/UserForm.vue')
-  },
-  {
-    path: '/list',
-    name: 'List',
-    component: () => import('../views/UserList.vue')
-  },
-  {
-    path: '/hello',
-    name: 'Hello',
-    component: Hello
   }
 ]
 
